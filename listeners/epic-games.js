2c18e490b0a589bbc46c3ac63a8651cdc2c2c358
Nicholas Kennedy <nmk10062@gmail.com>
2024-01-02 10:20:00 -0500

    Remove description from free games

--- listeners/epic-games.js
+++ listeners/epic-games.js
@@ -9,86 +9,72 @@ const schedule = require("node-schedule");
 
 module.exports = {
   run: (client) => {
-    schedule.scheduleJob("0 12 * * *", () => {
-      axios
-        .get(
-          "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=US&allowCountries=US"
-        )
-        .then(async (res) => {
-          const now = new Date();
-          const games = res.data.data.Catalog.searchStore.elements.filter(
-            (game) =>
-              game.expiryDate &&
-              now > new Date(game.effectiveDate) &&
-              now < new Date(game.expiryDate)
-          );
+    // schedule.scheduleJob("0 12 * * *", () => {
+    axios
+      .get(
+        "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=US&allowCountries=US"
+      )
+      .then(async (res) => {
+        const now = new Date();
+        const games = res.data.data.Catalog.searchStore.elements.filter(
+          (game) =>
+            game.expiryDate &&
+            now > new Date(game.effectiveDate) &&
+            now < new Date(game.expiryDate)
+        );
 
-          const freeGamesChannels = await FreeGamesChannels.findAll();
+        const freeGamesChannels = await FreeGamesChannels.findAll();
 
-          for (const game of games) {
-            axios
-              .get(
-                `https://store-content-ipv4.ak.epicgames.com/api/en-US/content/products/${game.productSlug}`
-              )
-              .then(async (res) => {
-                const page = res.data.pages.filter(
-                  (page) => page._slug === "home"
-                )[0];
+        for (const game of games) {
+          for (const freeGamesChannel of freeGamesChannels) {
+            const freeGame = await FreeGames.findOne({
+              where: { guild: freeGamesChannel.guild, game: game.id },
+            });
 
-                const description = page?.data.about.shortDescription;
+            if (freeGame) {
+              continue;
+            }
 
-                for (const freeGamesChannel of freeGamesChannels) {
-                  const freeGame = await FreeGames.findOne({
-                    where: { guild: freeGamesChannel.guild, game: game.id },
-                  });
+            const freeGamesRole = await FreeGamesRoles.findOne({
+              where: { guild: freeGamesChannel.guild },
+            });
 
-                  if (freeGame) {
-                    continue;
-                  }
+            const guild = client.guilds.cache.get(freeGamesChannel.guild);
+            const role = freeGamesRole
+              ? `${guild.roles.cache.get(freeGamesRole.role)}`
+              : "";
 
-                  const freeGamesRole = await FreeGamesRoles.findOne({
-                    where: { guild: freeGamesChannel.guild },
-                  });
+            client.channels
+              .fetch(freeGamesChannel.channel)
+              .then((channel) => {
+                const embed = new EmbedBuilder()
+                  .setColor("Blurple")
+                  .setTitle(game.title)
+                  .addFields({
+                    name: " ",
+                    value: `${bold("Free")} until ${time(
+                      new Date(game.expiryDate)
+                    )}`,
+                  })
+                  .setThumbnail("attachment://epic-games.png")
+                  .setImage(game.keyImages[0].url)
+                  .setURL(
+                    `https://store.epicgames.com/en-US/p/${game.productSlug}`
+                  );
 
-                  const guild = client.guilds.cache.get(freeGamesChannel.guild);
-                  const role = freeGamesRole
-                    ? `${guild.roles.cache.get(freeGamesRole.role)}`
-                    : "";
+                FreeGames.create({ guild: guild.id, game: game.id });
 
-                  client.channels
-                    .fetch(freeGamesChannel.channel)
-                    .then((channel) => {
-                      const embed = new EmbedBuilder()
-                        .setColor("Blurple")
-                        .setTitle(game.title)
-                        .setDescription(description)
-                        .addFields({
-                          name: " ",
-                          value: `${bold("Free")} until ${time(
-                            new Date(game.expiryDate)
-                          )}`,
-                        })
-                        .setThumbnail("attachment://epic-games.png")
-                        .setImage(game.keyImages[0].url)
-                        .setURL(
-                          `https://store.epicgames.com/en-US/p/${game.productSlug}`
-                        );
-
-                      FreeGames.create({ guild: guild.id, game: game.id });
-
-                      channel.send({
-                        content: role,
-                        embeds: [embed],
-                        files: ["./assets/images/logos/epic-games.png"],
-                      });
-                    })
-                    .catch(console.error);
-                }
+                channel.send({
+                  content: role,
+                  embeds: [embed],
+                  files: ["./assets/images/logos/epic-games.png"],
+                });
               })
               .catch(console.error);
           }
-        })
-        .catch(console.error);
-    });
+        }
+      })
+      .catch(console.error);
+    // });
   },
 };
