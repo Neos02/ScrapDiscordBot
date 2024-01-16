376af931ad6576dc3e474e61f007bafd89fce89f
Nicholas Kennedy <nmk10062@gmail.com>
2024-01-15 22:31:50 -0500

    Add stop command

---
+++ commands/music/stop.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { getVoiceConnection } = require("@discordjs/voice");
+const AudioQueue = require("../../utils/queue.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("stop")
+    .setDescription("Stops the music"),
+  async execute(interaction) {
+    if (!interaction.member.voice.channelId) {
+      return await interaction.reply({
+        content: "You must be in a voice channel to use this command!",
+        ephemeral: true,
+      });
+    }
+
+    if (!AudioQueue.isPlaying(interaction.guild.id)) {
+      return await interaction.reply({
+        content: "Nothing is playing right now",
+        ephemeral: true,
+      });
+    }
+
+    const connection = getVoiceConnection(interaction.guild.id);
+
+    AudioQueue.destroyPlayer(interaction.guild.id);
+    connection.destroy();
+
+    const embed = new EmbedBuilder()
+      .setColor("Blurple")
+      .setDescription("Music has been stopped");
+
+    return await interaction.reply({ embeds: [embed], ephemeral: true });
+  },
+};
